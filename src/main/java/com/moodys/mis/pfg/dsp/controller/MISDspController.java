75fce5d129c4d1af4317d0451cb30e890eecd076
Ashutosh Kumar Shuk;la <ashutosh.swd@gmail.com>
2023-04-28 18:24:36 +0530

    add logger

--- src/main/java/com/moodys/mis/pfg/dsp/controller/MISDspController.java
+++ src/main/java/com/moodys/mis/pfg/dsp/controller/MISDspController.java
@@ -2,6 +2,8 @@ package com.moodys.mis.pfg.dsp.controller;
 
 import com.moodys.mis.pfg.dsp.service.MIsService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MISDspController {
 
+private final Logger logger = LoggerFactory.getLogger(MISDspController.class);
     private final MIsService mIsService;
 
     @GetMapping("/hello")
     public String getData(){
+
+        logger.trace("Log level: TRACE");
+        logger.info("Log level: INFO");
+        logger.debug("Log level: DEBUG");
+        logger.error("Log level: ERROR");
+        logger.warn("Log level: WARN");
 
         return "hello data";
     }
--- src/main/java/com/moodys/mis/pfg/dsp/service/MIsService.java
+++ src/main/java/com/moodys/mis/pfg/dsp/service/MIsService.java
@@ -1,4 +1,7 @@
 package com.moodys.mis.pfg.dsp.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MIsService {
 }
